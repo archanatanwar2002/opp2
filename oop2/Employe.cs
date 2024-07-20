bd322349313654fb70a4d752ad4a562861522eab
archanatanwar2002 <archanatanwar2002@gmail.com>
2024-07-20 13:30:25 +0530

    Add files via upload

---
+++ oop2/Employe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    internal class Employe
+    {
+        //  field
+        private string name;
+        private double salary;
+
+        // constuctor
+        public Employe(string name, double salary)
+        // properities to acess field and  modify provate field
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0)
+                    salary = value;
+                else
+                {
+                    Console.WriteLine("salary cannot be negtative");
+                }
+            }
+        }
+    
+            public void DisplayEmployeInfo()
+                {
+                    Console.WriteLine($"Name:{Name}");
+                    Console.WriteLine($"salary:{Salary}");
+                }
+            }
+        } 
+
+
+    
+
---
+++ oop2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    internal class Program
+        // ENCASULATION
+    {
+        static void Main(string[] args)
+
+            //argument means constructor
+            // creat an employe  object
+
+        {
+            Employe employe1 = new Employe("shana ",300);
+            employe1.DisplayEmployeInfo();
+        }
+    }
+}
